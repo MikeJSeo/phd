5e1d4ab97de3dc6f93f2eb4fdb220a936f6792c8
Michael Seo <swj8874@gmail.com>
2021-09-19 00:12:59 +0200

    Update imputation code.R

--- oxford/imputation code.R
+++ oxford/imputation code.R
@@ -1,7 +1,7 @@
 library(jomo) # imputation package
 library(fastDummies) #to create dummy columns for treatment
 
-setwd("C:/Users/ms19g661/Desktop") #change directory
+setwd("C:/Users/mike/Desktop") #change directory
 data <- read.csv("data_for_new_imputation.csv")
 
 covariates <- c("AGE", "SEX", "HAMD_3", "HAMD_4", "HAMD_6", "HAMD_10", "HAMD_11", "HAMD_13", "HAMD_17", "HAMD_BASELINE",
@@ -16,10 +16,10 @@ data$STUDYID[data$STUDYID %in% c("29060/073", "29060/059")] <- "29060/059_073"
 data <- dummy_cols(data, select_columns = "TREATMENT_GROUP", remove_selected_columns = TRUE)
 covariates <- c(covariates, grep("TREATMENT_GROUP", colnames(data), value = TRUE))
 
-#convert cluster variable STUDYID to integer
+# Convert cluster variable STUDYID to integer
 newdata <- data[, c("STUDYID", covariates)]
 newdata$STUDYID <- as.numeric(as.factor(newdata$STUDYID))
-newdata[, c("STUDYID", covariates)] <- lapply(newdata[, c("STUDYID", covariates)], as.numeric)
+newdata[, covariates] <- lapply(newdata[, covariates], as.numeric)
 
 # The imputation model requires an intercept variable (simply a column of 1's)
 newdata$cons <- 1
@@ -29,9 +29,15 @@ X_imputation <- newdata[, c("cons", "AGE", "SEX", "HAMD_BASELINE", "AE_CATEGORY_
 clus <- newdata$STUDYID
 
 imp <- jomo(Y = y_imputation, X = X_imputation, clus = clus, meth = "common", nburn = 1000, nbetween = 1000, nimp = 5)
-#save(imp, file = "oxford-imputations.RData")
+save(imp, file = "oxford-imputations.RData")
 
+load("oxford-imputations.RData")
 library(mitools)
-imp.list <- imputationList(split(imp, imp$Imputation)[-1])
+#imp.list <- imputationList(split(imp, imp$Imputation)[-1])
+imp.list <- imputationList(split(imp, imp$Imputation))
+View(imp.list$imputations$`0`) #original data
+View(imp.list$imputations$`1`) #imputaed dataset 1
+
+
 
 
