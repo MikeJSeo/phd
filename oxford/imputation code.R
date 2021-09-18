310e7e59cd3bea4b4c1ae3a25ade68be411ecf47
Michael Seo <swj8874@gmail.com>
2021-09-18 02:05:05 +0200

    revision

--- oxford/imputation code.R
+++ oxford/imputation code.R
@@ -1,63 +1,44 @@
-#devtools::install_github("MikeJSeo/bipd") #please install this github - I put some functions in here for convenience
-library(bipd)
+library(jomo) # imputation package
 library(fastDummies) #to create dummy columns for treatment
 
-setwd("C:/Users/ms19g661/Desktop") #please change directory
+setwd("C:/Users/ms19g661/Desktop") #change directory
 data <- read.csv("data_for_new_imputation.csv")
 
 covariates <- c("AGE", "SEX", "HAMD_3", "HAMD_4", "HAMD_6", "HAMD_10", "HAMD_11", "HAMD_13", "HAMD_17", "HAMD_BASELINE",
-           #     "AE_CATEGORY_ABDOMINAL.PAIN.DISCOMFORT", "AE_CATEGORY_FATIGUE", "AE_CATEGORY_HEADACHE", "AE_CATEGORY_NAUSEA",
-          #      "AE_CATEGORY_SEDATION.SOMNOLENCE", "AE_CATEGORY_SEXUAL.DYSFUNCTION",
-                "HAMD_WEEK_1", "HAMD_WEEK_2", "HAMD_WEEK_3")#, "HAMD_WEEK_4", "HAMD_WEEK_5", "HAMD_WEEK_6", "HAMD_WEEK_7", "HAMD_WEEK_9", "HAMD_WEEK_10")
-#y <- "HAMD_WEEK_8"
+                "AE_CATEGORY_ABDOMINAL.PAIN.DISCOMFORT", "AE_CATEGORY_FATIGUE", "AE_CATEGORY_HEADACHE", "AE_CATEGORY_NAUSEA",
+                "AE_CATEGORY_SEDATION.SOMNOLENCE", "AE_CATEGORY_SEXUAL.DYSFUNCTION",
+                paste0("HAMD_WEEK_", 1:10))
 
 # Merge two studies
 data$STUDYID[data$STUDYID %in% c("29060/073", "29060/059")] <- "29060/059_073"
 
 # Change treatment to dummy variables
-table(data$STUDYID, data$TREATMENT_GROUP)
 data <- dummy_cols(data, select_columns = "TREATMENT_GROUP", remove_selected_columns = TRUE)
-#covariates <- c(covariates, grep("TREATMENT_GROUP", colnames(data), value = TRUE))
-
-# Define variable type
-#typeofvar <- c("continuous", "binary", rep("count", 7), "continuous",
-#               rep("binary", 6), rep("continuous", 9), rep("binary", 13))
-#typeofvar <- c("continuous", "binary", rep("count", 7), "continuous",
-#               rep("binary", 6), rep("continuous", 9))
-#typeofvar <- c("continuous", "binary", rep("count", 7), "continuous", rep("continuous", 9))
-typeofvar <- c("continuous", "binary", rep("count", 7), "continuous", rep("continuous", 3))
-
-names(typeofvar) <- covariates               
-              
-#create imputation related objects
-newdata <- data[, c("HAMD_WEEK_8", "STUDYID", covariates)]
-newdata[, c("STUDYID", covariates[typeofvar == "binary"])] <- lapply(newdata[,c("STUDYID", covariates[typeofvar == "binary"]), drop = FALSE], as.factor)
-
+covariates <- c(covariates, grep("TREATMENT_GROUP", colnames(data), value = TRUE))
 
 #convert cluster variable STUDYID to integer
-newdata$STUDYID <- as.numeric(newdata$STUDYID)
+newdata <- data[, c("STUDYID", covariates)]
+newdata$STUDYID <- as.numeric(as.factor(newdata$STUDYID))
+newdata[, c("STUDYID", covariates)] <- lapply(newdata[, c("STUDYID", covariates)], as.numeric)
 
-missingPattern <- findMissingPattern(newdata, covariates, studyname = "STUDYID")
-meth <- getCorrectMeth(newdata, missingPattern, outcomename = "HAMD_WEEK_8", studyname = "STUDYID", typeofvar = typeofvar, interaction = FALSE)
-#meth["HAMD_WEEK_8"] <- "2l.2stage.norm" 
-meth["HAMD_WEEK_8"] <- "2l.jomo" 
+# The imputation model requires an intercept variable (simply a column of 1's)
+newdata$cons <- 1
 
-pred <- getCorrectPred(newdata, missingPattern, outcomename = "HAMD_WEEK_8", studyname = "STUDYID", interaction = FALSE)
+y_imputation <- newdata[, c(paste0("HAMD_WEEK_", 1:10), "HAMD_3", "HAMD_4", "HAMD_6", "HAMD_10", "HAMD_11", "HAMD_13", "HAMD_17")]
+X_imputation <- newdata[, c("cons", "AGE", "SEX", "HAMD_BASELINE", "AE_CATEGORY_ABDOMINAL.PAIN.DISCOMFORT", "AE_CATEGORY_FATIGUE", "AE_CATEGORY_HEADACHE", "AE_CATEGORY_NAUSEA", "AE_CATEGORY_SEDATION.SOMNOLENCE", "AE_CATEGORY_SEXUAL.DYSFUNCTION")]
+clus <- newdata$STUDYID
 
-set.seed(1)
-imp <- mice(newdata, pred = pred, meth = meth)
+imp <- jomo(Y = y_imputation, X = X_imputation, clus = clus, meth = "common", nburn = 1000, nbetween = 1000, nimp = 5)
+#save(imp, file = "oxford-imputations.RData")
+
+library(mitools)
+imp.list <- imputationList(split(imp, imp$Imputation)[-1])
 
 
-# There are studies with too few observations: 29060/073 has 9
-table(data$STUDYID)
- 
-# merge studies
+# Define variable type
+typeofvar <- c("continuous", "binary", rep("continuous", 7), "continuous",
+               rep("binary", 6), rep("continuous", 10), rep("binary", 13))
+names(typeofvar) <- covariates               
 
-# Shows missing percentage for each variable in each study
-View(missingP$missingpercent)
 
-kable_out <- knitr::kable(missingP$missingpercent, "html")#%>% kableExtra::kable_styling(bootstrap_options = c("striped", "hover"))
-readr::write_file(kable_out, "kable_out.html")
-kable_out2 <- knitr::kable(table(data$STUDYID), "html")
-readr::write_file(kable_out2, "number of studies.html")
 
